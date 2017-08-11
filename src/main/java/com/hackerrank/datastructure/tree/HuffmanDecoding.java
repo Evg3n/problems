7d097b73749b6247ee15fc885f00ff4e0a84d200
Evg3nYo <evg3n.yo@gmail.com>
2017-08-10 21:13:22 -0400

    https://www.hackerrank.com/challenges/tree-huffman-decoding https://www.hackerrank.com/challenges/contacts

---
+++ src/main/java/com/hackerrank/datastructure/tree/HuffmanDecoding.java
@@ -0,0 +1,54 @@
+package com.hackerrank.datastructure.tree;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/tree-huffman-decoding
+ */
+public class HuffmanDecoding {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        Node b = new Node('B', 1);
+        Node c = new Node('C', 1);
+        Node bc = new Node('\0', 2, b, c);
+
+        Node a = new Node('A', 3);
+        Node root = new Node('\0', 5, bc, a);
+
+        decode(in.nextLine(), root);
+    }
+
+    private static void decode(String s, Node root) {
+        Node currentNode = root;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                currentNode = currentNode.left;
+            } else { // 1
+                currentNode = currentNode.right;
+            }
+            if (currentNode.data != '\0') {
+                System.out.print(currentNode.data);
+                currentNode = root;
+            }
+        }
+    }
+
+    static class Node {
+        char data;
+        int frequency;
+        Node left;
+        Node right;
+
+        Node(char data, int frequency) {
+            this.data = data;
+            this.frequency = frequency;
+        }
+
+        Node(char data, int frequency, Node left, Node right) {
+            this(data, frequency);
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ src/main/java/com/hackerrank/datastructure/trie/Contacts.java
@@ -0,0 +1,118 @@
+package com.hackerrank.datastructure.trie;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/contacts
+ */
+public class Contacts {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        Trie trie = new Trie();
+        for (int a0 = 0; a0 < n; a0++) {
+            String op = in.next();
+            String contact = in.next();
+
+            switch (op) {
+                case "add":
+                    trie.add(contact);
+                    break;
+                case "find":
+                    System.out.println(trie.find(contact));
+                    break;
+            }
+        }
+    }
+
+    private static class Trie {
+        private HashMap<Character, Node> roots;
+
+        Trie() {
+            roots = new HashMap<>();
+        }
+
+        void add(String value) {
+            char[] chars = value.toCharArray();
+
+            Node root = roots.computeIfAbsent(chars[0], k -> new Node(chars[0]));
+            addInTree(root, Arrays.copyOfRange(chars, 1, chars.length));
+        }
+
+        int find(String value) {
+            char[] chars = value.toCharArray();
+
+            if (!roots.containsKey(chars[0])) {
+                return 0;
+            }
+
+            Node traverseRoot = roots.get(chars[0]);
+            for (int i = 1; i < chars.length; i++) {
+                Map<Character, Node> children = traverseRoot.getChildren();
+                if (!children.containsKey(chars[i])) {
+                    return 0;
+                }
+                traverseRoot = children.get(chars[i]);
+            }
+
+            return traverseRoot.getEndingsCount();
+        }
+
+        private void addInTree(Node root, char[] chars) {
+            Node currentNode = root;
+            for (char c : chars) {
+                Map<Character, Node> children = currentNode.getChildren();
+                if (!children.containsKey(c)) {
+                    children.put(c, new Node(c));
+                }
+                currentNode.incrementEndingsCount();
+                currentNode = children.get(c);
+            }
+            currentNode.incrementEndingsCount();
+            currentNode.getChildren().put('\0', new Node('\0'));
+        }
+
+        private class Node {
+            private Character value;
+            private int endingsCount;
+
+            private Map<Character, Node> children;
+
+            Node(Character value) {
+                this.value = value;
+                endingsCount = 0;
+                children = new HashMap<>();
+            }
+
+            Character getValue() {
+                return value;
+            }
+
+            Map<Character, Node> getChildren() {
+                return children;
+            }
+
+            @Override
+            public int hashCode() {
+                return super.hashCode();
+            }
+
+            @Override
+            public boolean equals(Object obj) {
+                return value.equals(((Node) obj).getValue());
+            }
+
+            int getEndingsCount() {
+                return endingsCount;
+            }
+
+            void incrementEndingsCount() {
+                endingsCount++;
+            }
+        }
+    }
+}
