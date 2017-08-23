1b41635e48fc4135aa64058b74b4fcfe6e178b18
Evg3nYo <evg3n.yo@gmail.com>
2017-08-23 11:05:01 -0400

    Add more solutions.

---
+++ src/main/java/com/hackerrank/algorithm/string/GameOfThronesI.java
@@ -0,0 +1,33 @@
+package com.hackerrank.algorithm.string;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/game-of-thrones
+ */
+public class GameOfThronesI {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(GameOfThronesI.class.getClassLoader().getResourceAsStream(
+                "algorithm/string/game_of_thrones_i.txt"));
+
+        String s = in.next();
+        String result = gameOfThrones(s);
+        System.out.println(result);
+    }
+
+    private static String gameOfThrones(String s) {
+        int[] letterCounts = new int[26];
+        for (char c : s.toCharArray()) {
+            letterCounts[c - 'a'] += 1;
+        }
+
+        int singleLetterCount = 0;
+        for (int i = 0; i < 26; i++) {
+            if (letterCounts[i] % 2 == 1) {
+                singleLetterCount++;
+            }
+        }
+
+        return singleLetterCount <= 1 ? "YES" : "NO";
+    }
+}
