54e2e19a75edb4735235614036c114408413268b
Evg3nYo <evg3n.yo@gmail.com>
2017-11-11 15:49:49 -0500

    Add more solutions.

---
+++ src/main/java/com/acmp/Checkers2.java
@@ -0,0 +1,46 @@
+package com.acmp;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * http://acmp.ru/index.asp?main=task&id_task=684
+ * <p>
+ * Created by Yauheni Yudzitski <yauheni.yudzitski@gmail.com>
+ */
+public class Checkers2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        runLocal();
+    }
+
+    private static void runLocal() throws FileNotFoundException {
+        try (
+                Scanner sc = new Scanner(
+                        Checkers2.class.getClassLoader().getResourceAsStream("acmp/checkers_input.txt"));
+                PrintWriter out = new PrintWriter(new File("checkers_output.txt"))
+        ) {
+            solve(sc, out);
+        }
+    }
+
+    private static void runAcmp() throws FileNotFoundException {
+        try (
+                Scanner sc = new Scanner(new File("input.txt"));
+                PrintWriter out = new PrintWriter(new File("output.txt"))
+        ) {
+            solve(sc, out);
+        }
+    }
+
+    private static void solve(Scanner sc, PrintWriter out) {
+        String source = sc.next();
+        String target = sc.next();
+
+        int deltaX = target.charAt(0) - source.charAt(0);
+        int deltaY = target.charAt(1) - source.charAt(1);
+
+        out.write((deltaY - deltaX) % 2 == 0 && Math.abs(deltaX) <= deltaY ? "YES" : "NO");
+    }
+}
---
+++ src/main/java/com/acmp/Digits.java
@@ -0,0 +1,60 @@
+package com.acmp;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * http://acmp.ru/index.asp?main=task&id_task=303
+ * <p>
+ * Created by Yauheni Yudzitski <yauheni.yudzitski@gmail.com>
+ */
+public class Digits {
+    public static void main(String[] args) throws FileNotFoundException {
+        runLocal();
+    }
+
+    private static void runLocal() throws FileNotFoundException {
+        try (
+                Scanner sc = new Scanner(
+                        Digits.class.getClassLoader().getResourceAsStream("acmp/digits_input.txt"));
+                PrintWriter out = new PrintWriter(new File("digits_output.txt"))
+        ) {
+            solve(sc, out);
+        }
+    }
+
+    private static void runAcmp() throws FileNotFoundException {
+        try (
+                Scanner sc = new Scanner(new File("input.txt"));
+                PrintWriter out = new PrintWriter(new File("output.txt"))
+        ) {
+            solve(sc, out);
+        }
+    }
+
+    private static void solve(Scanner sc, PrintWriter out) {
+        String line = sc.next();
+
+        int[] left = new int[line.length()];
+        for (int i = 1; i < line.length(); i++) {
+            left[i] = left[i - 1] + (line.charAt(i - 1) - '0') * (i % 2 == 1 ? 1 : -1);
+        }
+
+        int[] right = new int[line.length()];
+        for (int i = line.length() - 2; i > -1; i--) {
+            right[i] = right[i + 1] + (line.charAt(i + 1) - '0') * (i % 2 == 1 ? -1 : 1);
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < line.length(); i++) {
+            int current = left[i] + right[i];
+            if (current > max) {
+                max = current;
+            }
+        }
+        out.write(Integer.toString(max));
+        out.close();
+    }
+}
